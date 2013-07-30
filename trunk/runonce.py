6268c54e8bd7e7053aca14627fd4c6ab418194ba
kidsshk3@gmail.com <kidsshk3@gmail.com@6595c7b5-07d9-f86e-bd0d-0fa38d3664a6>
2013-07-30 11:30:07 +0000

    change

--- trunk/runonce.py
+++ trunk/runonce.py
@@ -8,12 +8,12 @@ try:
 except:
     pass
 import os,sys,re,csv, subprocess, time, platform, logging, traceback, socket
-VER = 0
+VER = 1
 LOGGING_FORMAT = '[%(asctime)-15s] %(message)s'
 
 logging.basicConfig(filename='/home/cdnlab-gnp/gnp-logs/runonce_%s.log'%socket.gethostname(), level='DEBUG', format=LOGGING_FORMAT) 
 try:
-    rhdl = open('/home/cdnlab-gnp/gnp-logs/runonce_%s.lock'%socket.gethostname(), 'r') 
+    rhdl = open('/home/cdnlab-gnp/runonce_%s.lock'%socket.gethostname(), 'r') 
     cur_ver = int(rhdl.read().strip())
     if cur_ver >= VER:
         exit(0)
@@ -22,10 +22,15 @@ except SystemExit:
 except:
     logging.exception(''.join(traceback.format_exception(*sys.exc_info())))
     pass
-os.system('/usr/bin/mysql -u root -paQcy7j2CSHYhDB8E cdnlab < /home/cdnlab-gnp/gnp-deploy/import-nodes.sql >> mysql_%s.log'%socket.gethostname())
+
+os.system('rm -fR /home/cdnlab-gnp/gnp-logs')
+os.system('svn checkout https://gnp-deploy.googlecode.com/svn/branches/logs/%s /home/cdnlab-gnp/gnp-logs --username shk3@monkeyhouse.info --password pu8bq7qu5hB4'%socket.gethostname())
+os.system('/usr/bin/mysql -u root -paQcy7j2CSHYhDB8E cdnlab < /home/cdnlab-gnp/gnp-deploy/import-nodes.sql >> /home/cdnlab-gnp/gnp-logs/mysql_%s.log'%socket.gethostname())
+
+# os.system('md /home/cdnlab-gnp/gnp-logs/%s'%socket.gethostname())
 
 logging.info('Run at version %d.' % VER)
-whdl = open('/home/cdnlab-gnp/gnp-logs/runonce_%s.lock'%socket.gethostname(), 'w')
+whdl = open('/home/cdnlab-gnp/runonce_%s.lock'%socket.gethostname(), 'w')
 print(VER, file=whdl)
 whdl.close()
 logging.info('Lock of version %d is saved.' % VER)
