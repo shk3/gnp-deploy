de02b19aedb8c040566b72c14a1a370ad5ab42f4
kidsshk3@gmail.com <kidsshk3@gmail.com@6595c7b5-07d9-f86e-bd0d-0fa38d3664a6>
2013-07-31 01:30:53 +0000

    Do not show information of script updating, and give runonce log.

--- cron/updateGNP.sh
+++ cron/updateGNP.sh
@@ -1,3 +1,3 @@
-svn checkout http://gnp-deploy.googlecode.com/svn/trunk/ /home/cdnlab-gnp/gnp-deploy
+svn checkout http://gnp-deploy.googlecode.com/svn/trunk/ /home/cdnlab-gnp/gnp-deploy --non-interactive -q
 cd /home/cdnlab-gnp/gnp-logs && svn add *.log -q && svn ci *.log -m "auto upload" --username shk3@monkeyhouse.info --password pu8bq7qu5hB4 --non-interactive -q
-cd /home/cdnlab-gnp && python /home/cdnlab-gnp/gnp-deploy/runonce.py
+cd /home/cdnlab-gnp && python /home/cdnlab-gnp/gnp-deploy/runonce.py >> /home/cdnlab-gnp/gnp-logs/runonce.log
