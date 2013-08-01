d68241ba28c1cee2c9ae5ab7ebe2f5f1942f3790
kidsshk3@gmail.com <kidsshk3@gmail.com@6595c7b5-07d9-f86e-bd0d-0fa38d3664a6>
2013-08-01 07:43:37 +0000

    Reduce the ping amount and auto-save interval.

--- cron/runScanner.sh
+++ cron/runScanner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . ~/.bash_profile
-cd /home/cdnlab-gnp && python /home/cdnlab-gnp/gnp-deploy/obtain_ipaddr.py 100 30 10 &
+cd /home/cdnlab-gnp && python /home/cdnlab-gnp/gnp-deploy/obtain_ipaddr.py 25 30 30 &
