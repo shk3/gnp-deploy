675d708e021b5fc7248d973c848002a810213f4d
kidsshk3@gmail.com <kidsshk3@gmail.com@6595c7b5-07d9-f86e-bd0d-0fa38d3664a6>
2013-08-05 10:23:14 +0000

    Pause the scanners.

--- trunk/cron/runScanner.sh
+++ trunk/cron/runScanner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . ~/.bash_profile
-cd /home/cdnlab-gnp && python /home/cdnlab-gnp/gnp-deploy/obtain_ipaddr.py 25 30 30 &
+# cd /home/cdnlab-gnp && python /home/cdnlab-gnp/gnp-deploy/obtain_ipaddr.py 25 30 30 &
